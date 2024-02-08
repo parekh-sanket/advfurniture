f6b12880ed51841b80549bff1754b11bead7a551
sanket <sanketparekh17@gmail.com>
2024-02-08 21:42:48 +0530

    first push

---
+++ src/app/home/page.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (<>
+        <h1>
+            Veniam proident officia nisi pariatur commodo. Sunt ipsum in aute cupidatat laborum et. Ullamco anim occaecat culpa duis qui eu minim officia dolor labore. Non dolor labore enim consequat duis ipsum commodo cillum. Elit eu occaecat quis deserunt. Quis fugiat do tempor voluptate non cupidatat commodo dolore adipisicing nostrud adipisicing aute. Est labore ullamco commodo non enim.
+        </h1>
+    </>)
+}
+
+export default HomePage
